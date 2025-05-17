d3368b1267dd133dce1a59dce8f48bd01a57bcc6
baim <rizkikasim224@gmail.com>
2025-05-18 02:56:39 +0700

    menambahkan fitur produk

--- projek/js/script.js
+++ projek/js/script.js
@@ -1,16 +1,74 @@
-// toggle class active
+// toggle class active hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
+
+// Toggle class 'active' pada search form
+const searchForm = document.querySelector('.search-form');
+const searchButton = document.querySelector('#search-button');
+const searchBox = document.querySelector('#search-box'); // ✅ Tambahkan ini!
+
+searchButton.onclick = (event) => {
+  event.preventDefault(); // selalu taruh di atas agar mencegah default lebih awal
+  searchForm.classList.toggle('active');
+  searchBox.focus(); // pastikan searchBox sudah didefinisikan
+};
+
 
 // ketika hamburger menu diklik
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+// Toggle clas active untuk shopping card
+const shoppingCart = document.querySelector('.shopping-card');
+
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
+
+
+
+// klik diluar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function(e){
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
     }
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)){
+        searchForm.classList.remove('active');
+    }
+    if(!sc.contains(e.target) && !shoppingCart.contains(e.target)){
+        shoppingCart.classList.remove('active');
+    }
 });
+
+// modal box
+
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButton = document.querySelectorAll('.item-detail-button');
+
+itemDetailButton.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+};
+});
+
+
+
+// klik tombol close
+document.querySelector('.modal .close-icon').onclick = (e) =>{
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+}
+
+// klick diluar modal
+window.onclick = (e) => {
+  if(e.target === itemDetailModal) {
+    modal.style.display = 'none';
+  }
+}
